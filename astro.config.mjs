f3c5bd399968ef77b0e43b7392051ab5b3a28aff
franruiloz-lab <franruiloz-lab@users.noreply.github.com>
2026-03-05 21:03:24 +0100

    Añadir @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // Custom domain active — update CNAME file and this site if domain changes
 export default defineConfig({
   site: 'https://timeofpray.net',
   base: '/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
